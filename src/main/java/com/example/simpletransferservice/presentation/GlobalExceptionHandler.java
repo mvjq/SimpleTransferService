0e88f4fb2e495276fc4e4189b983bcf4c6ca2066
mvjq <mviniciusjunqueira@gmail.com>
2025-10-22 01:19:47 -0300

    Added GlobalExceptionHandler e

---
+++ src/main/java/com/example/simpletransferservice/presentation/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.simpletransferservice.presentation;
+
+
+import com.example.simpletransferservice.domain.exception.UserNotFoundException;
+import com.example.simpletransferservice.domain.exception.WalleNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({UserNotFoundException.class, WalleNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(
+            UserNotFoundException ex,
+            WebRequest request) {
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        body.put("path", request.getDescription(false).replace("uri=", ""));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    }
+}
