7ccf0c7caf376b7fe02e24390c8e16f250a79bfc
mvjq <mviniciusjunqueira@gmail.com>
2025-10-21 21:17:55 -0300

    enhance transaction status management with transition methods and default status

--- src/main/java/com/example/simpletransferservice/domain/enums/TransactionStatus.java
+++ src/main/java/com/example/simpletransferservice/domain/enums/TransactionStatus.java
@@ -1,4 +1,45 @@
 package com.example.simpletransferservice.domain.enums;
 
-public class TransactionStatus {
+import com.example.simpletransferservice.domain.model.Transaction;
+
+import javax.security.auth.login.FailedLoginException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public enum TransactionStatus {
+    PENDING,
+    VALIDATING,
+    AUTHORIZED,
+    PROCESSING,
+    COMPLETED,
+    AUTH_DENIED,
+    FAILED,
+    REVERSED;
+
+    private static final Map<TransactionStatus, Set<TransactionStatus>> TRANSITIONS = new HashMap<>();
+
+    static {
+        TRANSITIONS.put(PENDING, Set.of(VALIDATING));
+        TRANSITIONS.put(VALIDATING, Set.of(AUTHORIZED, AUTH_DENIED));
+        TRANSITIONS.put(AUTHORIZED, Set.of(PROCESSING));
+        TRANSITIONS.put(PROCESSING, Set.of(COMPLETED, FAILED));
+        TRANSITIONS.put(COMPLETED, Set.of(REVERSED));
+        TRANSITIONS.put(AUTH_DENIED, Set.of());
+        TRANSITIONS.put(FAILED, Set.of());
+        TRANSITIONS.put(REVERSED, Set.of());
+    }
+
+    public Set<TransactionStatus> getPossibleTransitions() {
+        return TRANSITIONS.getOrDefault(this, Set.of());
+    }
+
+    public boolean canTransitionTo(TransactionStatus newStatus) {
+        return getPossibleTransitions().contains(newStatus);
+    }
+
+    public boolean isFinal() {
+        return getPossibleTransitions().isEmpty();
+    }
+
 }
--- src/main/java/com/example/simpletransferservice/domain/model/Transaction.java
+++ src/main/java/com/example/simpletransferservice/domain/model/Transaction.java
@@ -23,8 +23,31 @@ public class Transaction {
     private Long payeeId;
     @DecimalMin(value = "0.01")
     private BigDecimal amount;
-    private TransactionStatus status;
+    @Builder.Default
+    private TransactionStatus status =TransactionStatus.PENDING;
     private String failureReason;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+
+    public void transitionTo(TransactionStatus newStatus) {
+        if (status.canTransitionTo(newStatus)) {
+            failureReason = String.format("Invalid status transition from %s to %s", status, newStatus);
+            throw new IllegalStateException(failureReason);
+        }
+
+        this.status = newStatus;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void transitionWithFailureReason(TransactionStatus newStatus, String reason) {
+        if (status.canTransitionTo(newStatus)) {
+            failureReason = String.format("Invalid status transition from %s to %s", status, newStatus);
+            throw new IllegalStateException(failureReason);
+        }
+
+        this.status = newStatus;
+        this.failureReason = reason;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
